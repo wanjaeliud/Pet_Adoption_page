2ec6ae316666287c6d0a131194b700bd75d9606d
“wanjaeliud” <eliudwys@students.ku.ac.ke>
2022-01-23 16:19:06 +0300

    json files

--- src/Pet.js
+++ src/Pet.js
@@ -4,7 +4,7 @@ const Pet = (props) => {
   return (
     <div>
       <h2>{props.name}</h2>
-      <h3>{props.animal}</h3>
+      <h3>{props.car}</h3>
       <h3>{props.bread}</h3>
     </div>
   );
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,38 @@
+import { useState } from "react";
+
+const CARS = ["tesla", "volkswagen", "audi", "merceds-benz", "toyata"];
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+  const [car, setCar] = useState("");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="location" />
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="location"
+          />
+        </label>
+        <label htmlFor="car">
+          Car
+          <select
+            id="car"
+            value={car}
+            onChange={(e) => setCar(e.target.value)}
+            onBlur={(e) => setCar(e.target.value)}
+          >
+            <option />
+            {CARS.map((car) => (
+              <option value={car} key={car}>
+                {car}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
